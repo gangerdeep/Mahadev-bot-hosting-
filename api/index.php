f1a296667dbe62c730a2689f6302e14b2137ecba
SANDEEP <sandeepbhabhar00@gmail.com>
2026-03-09 05:20:43 +0530

    Change Telegram bot token
    Updated the bot token for the Telegram API.

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$botToken = "8525855467:AAEgyjEhpB75XMvwmSfXa-HJY-BeeFL1Rag";
+$botToken = "8525855467:AAE5TJaKvceJ632XygoP_Zq8Jaz4TXL00xM";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $update = json_decode(file_get_contents("php://input"), true);
